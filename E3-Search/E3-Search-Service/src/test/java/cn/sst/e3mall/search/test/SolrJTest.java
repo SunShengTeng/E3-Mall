8d0bb454b7465ca7a99c3fedcbdf6b42d814ca9a
sunshengteng <sunshengteng@yahoo.com>
2018-03-20 22:19:42 +0800

    add:add init search service

---
+++ E3-Search/E3-Search-Service/src/test/java/cn/sst/e3mall/search/test/SolrJTest.java
@@ -0,0 +1,29 @@
+package cn.sst.e3mall.search.test;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * @author sunshengteng
+ *
+ */
+public class SolrJTest {
+	private SolrClient getHttpSolrJClient() {
+
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:Spring/ApplicationContext-Service.xml");
+		return (SolrClient) applicationContext.getBean("httpSolrClient");
+	}
+
+	@Test
+	public void testSolrJ() throws Exception {
+
+		SolrClient solrClient = getHttpSolrJClient();
+		solrClient.deleteByQuery("*:*");
+		solrClient.commit();
+
+	}
+
+}
